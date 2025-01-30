cbde31b4724b1fe827cba6098e7990eb98cbb990
Tu Nombre <tuemail@dominio.com>
2025-01-29 22:59:39 -0500

    get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('print-json')
+  getJson2() {
+    return { message: 'Petición recibida' };
+  }
+  @Get()
+  getJson3() {
+    return { message: 'Petición recibida' };
+  }
 
   @Post('print-json')
   printJson(@Body() body: any) {
