7ba5166923011fa7c08fd4e05e3aa60dbbf2a439
David Zamora <d.zamorac@uniandes.edu.co>
2025-07-15 23:05:46 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['https://production-control.vercel.app'],
+    methods: 'GET,POST,PATCH,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(3000);
 }
