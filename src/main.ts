3b261a6ed6627e8206ed2558459227e51d4c5ec9
David Zamora <d.zamorac@uniandes.edu.co>
2025-07-16 07:21:08 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://production-control.vercel.app'],
-    methods: 'GET,POST,PATCH,DELETE',
+    methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
