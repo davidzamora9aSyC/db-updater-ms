ac68f3e5eb1187856d22a97240fd61525d6bdab2
David Zamora <d.zamorac@uniandes.edu.co>
2025-07-21 07:47:15 -0500

    .

--- src/registro-minuto/registro-minuto.module.ts
+++ src/registro-minuto/registro-minuto.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { RegistroMinutoService } from './registro-minuto.service'
 import { RegistroMinuto } from './registro-minuto.entity'
+import { RegistroMinutoController } from './registro-minuto.controller'
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([RegistroMinuto])],
   providers: [RegistroMinutoService],
-  controllers: [],
+  controllers: [RegistroMinutoController],
 })
 export class RegistroMinutoModule {}
