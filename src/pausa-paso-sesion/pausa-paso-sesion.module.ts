e138c26572d2243189efa04ffa95e4cd9bb6b8d9
davidzamora9aSyC <79547839+davidzamora9aSyC@users.noreply.github.com>
2025-08-12 12:33:54 -0500

    fix: provide PasoProduccionService to pause session

--- src/pausa-paso-sesion/pausa-paso-sesion.module.ts
+++ src/pausa-paso-sesion/pausa-paso-sesion.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PausaPasoSesion } from './pausa-paso-sesion.entity';
 import { PausaPasoSesionService } from './pausa-paso-sesion.service';
+import { PasoProduccionModule } from '../paso-produccion/paso-produccion.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PausaPasoSesion])],
+  imports: [TypeOrmModule.forFeature([PausaPasoSesion]), PasoProduccionModule],
   providers: [PausaPasoSesionService],
   exports: [PausaPasoSesionService],
 })
