5fe411f77085c67c99e208d9eec81320b5360a3b
David Zamora <d.zamorac@uniandes.edu.co>
2025-08-12 14:02:14 -0500

    ,

--- src/sesion-trabajo/sesion-trabajo.module.ts
+++ src/sesion-trabajo/sesion-trabajo.module.ts
@@ -5,10 +5,14 @@ import { SesionTrabajoService } from './sesion-trabajo.service';
 import { SesionTrabajoController } from './sesion-trabajo.controller';
 import { RegistroMinutoModule } from '../registro-minuto/registro-minuto.module';
 import { EstadoSesionModule } from '../estado-sesion/estado-sesion.module';
+import { EstadoSesion } from '../estado-sesion/estado-sesion.entity';
+import { EstadoTrabajador } from '../estado-trabajador/estado-trabajador.entity';
+import { EstadoMaquina } from '../estado-maquina/estado-maquina.entity';
+import { RegistroMinuto } from '../registro-minuto/registro-minuto.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([SesionTrabajo]),
+    TypeOrmModule.forFeature([EstadoSesion, EstadoTrabajador, EstadoMaquina, SesionTrabajo, RegistroMinuto]),
     RegistroMinutoModule,
     EstadoSesionModule,
   ],
